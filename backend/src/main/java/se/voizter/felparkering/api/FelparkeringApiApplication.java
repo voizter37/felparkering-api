9a37ba7ce34707f07dcb73fa2602952f3d01cf86
Johan <isacsson.johan@gmail.com>
2025-06-12 22:28:40 +0200

    init spring boot backend

---
+++ backend/src/main/java/se/voizter/felparkering/api/FelparkeringApiApplication.java
@@ -0,0 +1,13 @@
+package se.voizter.felparkering.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelparkeringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelparkeringApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/voizter/felparkering/api/FelparkeringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.voizter.felparkering.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelparkeringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

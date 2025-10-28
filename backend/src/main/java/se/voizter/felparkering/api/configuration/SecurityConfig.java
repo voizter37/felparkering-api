ad2f967dcd01b49aa6251b6f158411a605190d67
Johan <isacsson.johan@gmail.com>
2025-10-28 17:57:20 +0100

    Making the CORS config primary

--- backend/src/main/java/se/voizter/felparkering/api/configuration/SecurityConfig.java
+++ backend/src/main/java/se/voizter/felparkering/api/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -73,7 +74,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
+    @Primary
     CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowedOriginPatterns(List.of(
